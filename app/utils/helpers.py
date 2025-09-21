58ea9b43853676082852fb834d7659bf282dfc9d
timothyckl <timothy.ckl@outlook.com>
2025-09-21 22:14:50 +0800

    Added parser functions

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -2,13 +2,12 @@
 helpers.py
 
 This module contains general-purpose utility functions and helpers used across
-the application. 
+the application.
 
 The module is intended to provide reusable tools that simplify data ingestion,
 processing, and analysis workflows. Additional helper functions may be added
 as the application evolves.
 """
-
 
 from datetime import date
 from functools import wraps
@@ -16,6 +15,8 @@ from time import time
 from typing import Callable, ParamSpec, TypeVar
 
 import pandas as pd
+import yfinance as yf
+from models.base import Ticker
 
 
 def n_year_window(n: int, timezone: str = "Asia/Singapore") -> tuple[date, date]:
@@ -49,6 +50,7 @@ def timer(func: Callable[P, R]) -> Callable[P, R]:
         A wrapped function that behaves like `func` but prints the time
         it took to execute.
     """
+
     @wraps(func)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
         """Wrapper function that times the execution of `func`."""
@@ -57,9 +59,30 @@ def timer(func: Callable[P, R]) -> Callable[P, R]:
         end = time()
         print(f"[INFO] {func.__name__} took {end - start:.6f} seconds.")
         return output
+
     return wrapper
 
 
-def yf_to_model():
-    """Parses yfinance classes into data models"""
-    pass
+def yf_ticker_to_model(symbol: str) -> Ticker:
+    """Converts a yfinance Ticker into a structured Ticker model."""
+    yf_ticker = yf.Ticker(symbol)
+    info = yf_ticker.info
+
+    return Ticker(
+        symbol=symbol,
+        name=info["shortName"],
+        market_cap=info["marketCap"],
+        price=info["currentPrice"],
+        sector=info["sector"],
+        industry=info["industry"],
+    )
+
+
+def yf_download_to_model(
+    symbols: str | list[str], start: str, end: str, interval: str = "1d", **kwargs
+) -> pd.DataFrame:
+    data = yf.download(
+        tickers=symbols, start=start, end=end, interval=interval, **kwargs
+    )
+
+    return data
