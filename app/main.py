4d4f2e8e52c986257047bf573d7a7fadf986d121
timothyckl <timothy.ckl@outlook.com>
2025-09-21 22:10:00 +0800

    Updated main.py to use streamlit

--- app/main.py
+++ app/main.py
@@ -8,36 +8,19 @@ Note:
     eventually reorganising appropriately.
 """
 
-from pprint import pprint
-from services.data import load_mapping
-from services.finance import get_ticker, get_top_performers_by_industry
-from utils.helpers import n_year_window, timer
+import streamlit as st
+from services.finance import get_ticker_data, get_ticker_info
+from utils.helpers import timer
 
 
 @timer
 def main() -> None:
-    sector_map = load_mapping()
-    tech = sector_map["technology"]
-    software = tech[3]
-    performers = get_top_performers_by_industry(software)
+    ticker = st.text_input(label="Enter Ticker...", value="NVDA")
+    info = get_ticker_info(ticker_symbol=ticker)
+    df = get_ticker_data(ticker_symbols=ticker, auto_adjust=True, progress=False)
 
-    print(software)
-    print(f"{performers}\n")
-
-    # start, end = n_year_window(n=3)
-    # print(f"start: {start}, end: {end}\n")
-    #
-    # for tkr in performers:
-    #     data = get_ticker_data(
-    #         tickers=tkr, start=start, end=end, auto_adjust=True, progress=False
-    #     )
-    #
-    #     print(f"{tkr}: {len(data)}")
-    #     # print(data.describe())
-    #     # break
-    #     print()
-    # # pprint(sector_map)
-    # pprint(sector_map)
+    st.write(info) 
+    st.write(df) 
 
 
 if __name__ == "__main__":
