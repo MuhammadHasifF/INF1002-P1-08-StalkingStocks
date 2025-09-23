cfbbbbdecfdc8e7164eb47dbc1ba99c127a2aba3
timothyckl <timothy.ckl@outlook.com>
2025-09-23 13:48:05 +0800

    Updated with testing code here

--- app/main.py
+++ app/main.py
@@ -9,19 +9,30 @@ Note:
 """
 
 import streamlit as st
-from services.finance import get_ticker_data, get_ticker_info
+from services.finance import get_sector_data, get_sectors, get_industry_data, get_ticker_data, get_ticker_info
 from utils.helpers import timer
-
+from tqdm import tqdm
 
 @timer
 def main() -> None:
-    ticker = st.text_input(label="Enter Ticker...", value="NVDA")
-    info = get_ticker_info(ticker_symbol=ticker)
-    df = get_ticker_data(ticker_symbols=ticker, auto_adjust=True, progress=False)
+    # ticker = st.text_input(label="Enter Ticker...", value="NVDA")
+    # info = get_ticker_info(ticker_symbol=ticker)
+    # df = get_ticker_data(ticker_symbols=ticker, auto_adjust=True, progress=False)
+    # st.write(info) 
+    # st.write(df) 
+    
+    # sectors = get_sectors()
+    #
+    # for s in tqdm(sectors):
+    #     data = get_sector_data(sector_key=s)
+    #     st.write(data.industries)
+    #     break
 
-    st.write(info) 
-    st.write(df) 
-
+    data = get_industry_data('software-application')
+    st.subheader("Top Performing")
+    st.write(data.top_performing)
+    st.subheader("Top Growing")
+    st.write(data.top_growing)
 
 if __name__ == "__main__":
     main()
