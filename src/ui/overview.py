9e46d9d16b438b0bd6283306b6087a219b381ef2
timothyckl <timothy.ckl@outlook.com>
2025-10-08 13:33:32 +0800

    Removed duplicate display_filters function

--- src/ui/overview.py
+++ src/ui/overview.py
@@ -10,7 +10,7 @@ from src.services.finance import (get_industry_info, get_ticker_data,
 from src.ui.charts import (add_indicators, create_figure, set_candlechart,
                            set_line_trend_chart, set_linechart, set_treemap)
 from src.utils.helpers import (format_date, format_large_number, format_name,
-                               rolling_window, timer)
+                               timer)
 
 
 def display_sector_overview(column, sector_data) -> None:
@@ -60,83 +60,6 @@ def display_industry_overview(column, industries) -> None:
     fig = set_treemap(summary_df)
 
     column.plotly_chart(fig, use_container_width=True)
-
-
-def display_filters(column, top_companies) -> dict[str, Any]:
-    selected_ticker = column.selectbox(
-        "Select a ticker",
-        top_companies,
-        help="Choose the stock or asset you want to view.",
-    )
-
-    horizon_mapping = {
-        "1 Day": {"n": 1, "unit": "days"},  # interval = 1m
-        "5 Day": {"n": 5, "unit": "days"},
-        "1 Month": {"n": 1, "unit": "months"},
-        "6 Month": {"n": 6, "unit": "months"},
-        "1 Year": {"n": 1, "unit": "years"},
-        "3 Year": {"n": 3, "unit": "years"},
-        "5 Year": {"n": 5, "unit": "years"},
-    }
-
-    selected_key = column.pills(
-        "Time Horizon",
-        options=list(horizon_mapping.keys()),
-        default="1 Year",
-        help="Select how far back the data should be shown.",
-    )
-
-    selected_horizon = horizon_mapping[selected_key]
-
-    # i lazy fix the error here AHHHHHH
-    if selected_horizon["unit"] == "days":
-        data_intervals = ["1m", "2m", "5m", "15m", "30m", "1h"]
-    elif selected_horizon["unit"] == "days" and selected_horizon["n"] == 5:
-        data_intervals = ["1m", "2m", "5m", "15m", "30m", "1h", "1d"]
-    else:
-        data_intervals = ["1d", "5d", "1wk", "1mo", "3mo"]
-
-    selected_interval = column.pills(
-        "Data Interval",
-        data_intervals,
-        default=data_intervals[0],
-        help="Select how often data points are sampled (e.g. daily, weekly, or hourly).",
-    )
-
-    indicator_mapping = {"SMA 5": 5, "SMA 20": 20, "SMA 50": 50}
-
-    tech_indicators = column.multiselect(
-        "Technical Indicators",
-        options=list(indicator_mapping.keys()),
-        default=[],
-        help="Apply indicators that reveal trends, momentum, and market strength.",
-    )
-    selected_indicators = [indicator_mapping[ind] for ind in tech_indicators]
-
-    selected_chart_type = column.radio(
-        "Select a chart",
-        options=["Line Chart", "Line Chart and Trend Markers", "Candlestick Chart"],
-        captions=[
-            "Simple view of how values change over time.",
-            "Line chart with up/down trend runs.",
-            "Shows price highs, lows, open, and close.",
-        ],
-        index=0,
-        help="Choose how you’d like the data displayed.",
-    )
-
-    start, end = rolling_window(**selected_horizon)
-
-    # can change to dataclass/basemodel
-    filters = {
-        "selected_ticker": selected_ticker,
-        "selected_horizon": {"start": start, "end": end},
-        "selected_interval": selected_interval,
-        "selected_indicators": selected_indicators,
-        "selected_chart_type": selected_chart_type,
-    }
-
-    return filters
 
 
 @st.cache_data
