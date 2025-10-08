a88127bdf2e827990d32083f84e976825a51e8e6
timothyckl <timothy.ckl@outlook.com>
2025-10-08 12:41:12 +0800

    Revised imports

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ dashboard.py
 
 This is the main entry point for the finance dashboard application.
 """
-from app.dashboard import run_dashboard 
+from src.dashboard import run_dashboard 
 
 if __name__ == "__main__":
     run_dashboard()
